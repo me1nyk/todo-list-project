2d6b4891c419e7269858d1ed11ebaf8f06b69823
Serhii Melnyk <serg.melnyk1998@gmail.com>
2023-04-03 14:46:51 +0300

    Update settings for using bootsrtap and crispy

--- todo_list/settings.py
+++ todo_list/settings.py
@@ -37,7 +37,9 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
-    "todo"
+    "todo",
+    "crispy_forms",
+    "crispy_bootstrap4",
 ]
 
 MIDDLEWARE = [
@@ -126,3 +128,5 @@ STATIC_ROOT = BASE_DIR / "staticfiles"
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+CRISPY_TEMPLATE_PACK = "bootstrap4"
