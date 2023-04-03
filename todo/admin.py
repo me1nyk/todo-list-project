06ccacd54a73227cabd7ad3f44aa419857dd97f5
Serhii Melnyk <serg.melnyk1998@gmail.com>
2023-04-03 11:57:48 +0300

    Customize admin panel

--- todo/admin.py
+++ todo/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
 
-# Register your models here.
+from todo.models import Tag, Task
+
+
+class TaskAdmin(admin.ModelAdmin):
+    list_display = ["content", "created_at", "deadline", "is_done"]
+    list_filter = ["is_done"]
+    search_fields = ["tags"]
+
+
+admin.site.register(Tag)
+admin.site.register(Task, TaskAdmin)
+
