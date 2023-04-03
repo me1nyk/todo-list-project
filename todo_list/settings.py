5a73bf6182597b5bea9e9cb35f9e3a3b68b677b8
Serhii Melnyk <serg.melnyk1998@gmail.com>
2023-04-03 12:13:40 +0300

    Modify settings to use static

--- todo_list/settings.py
+++ todo_list/settings.py
@@ -118,6 +118,10 @@ USE_TZ = False
 
 STATIC_URL = "static/"
 
+STATICFILES_DIRS = (BASE_DIR / "static",)
+
+STATIC_ROOT = BASE_DIR / "staticfiles"
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
