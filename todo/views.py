5d162454ac93052ea4e95b5e4830acf6814be34d
Serhii Melnyk <serg.melnyk1998@gmail.com>
2023-04-03 14:48:51 +0300

    Update views.py, implement update, create, delete views and task_remove_or_assign

--- todo/views.py
+++ todo/views.py
@@ -1,13 +1,66 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, redirect
+from django.urls import reverse_lazy
+from django.views import generic
 
-from todo.models import Task
+from todo.models import Task, Tag
 
 
-def index(request):
-    tasks = Task.objects.all()
+class TaskListView(generic.ListView):
+    model = Task
+    template_name = "todo/index.html"
+    context_object_name = "tasks"
 
-    context = {
-        "tasks": tasks,
-    }
 
-    return render(request, 'todo/index.html', context=context)
+class TaskCreateView(generic.CreateView):
+    model = Task
+    template_name = "todo/task_form.html"
+    fields = ("content", "deadline", "tags")
+    success_url = reverse_lazy("todo:index")
+
+
+class TaskUpdateView(generic.UpdateView):
+    model = Task
+    template_name = "todo/task_form.html"
+    fields = ("content", "deadline", "tags")
+    success_url = reverse_lazy("todo:index")
+
+
+class TaskDeleteView(generic.DeleteView):
+    model = Task
+    template_name = "todo/task_confirm_delete.html"
+    success_url = reverse_lazy("todo:index")
+
+
+class TagListView(generic.ListView):
+    model = Tag
+    template_name = "todo/tag_list.html"
+    context_object_name = "tags"
+
+
+class TagCreateView(generic.CreateView):
+    model = Tag
+    fields = ["name"]
+    success_url = reverse_lazy("todo:tag-list")
+    template_name = "todo/tag_form.html"
+
+
+class TagUpdateView(generic.UpdateView):
+    model = Tag
+    fields = ["name"]
+    template_name = "todo/tag_form.html"
+
+
+class TagDeleteView(generic.DeleteView):
+    model = Tag
+    success_url = reverse_lazy("todo:tag-list")
+    template_name = "todo/tag_confirm_delete.html"
+
+
+def task_remove_or_assign(request, pk):
+    task = get_object_or_404(Task, pk=pk)
+    if task.is_done:
+        task.is_done = False
+    else:
+        task.is_done = True
+    task.save()
+    return redirect('todo:index')
